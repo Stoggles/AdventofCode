8aab6def9a2e42f9345c2342e15384271c1c78b4
Adam Barker <mail@steg.pro>
2018-12-05 11:59:05 +0000

    day05 🌟🌟

--- 2018/Sources/2018/day05.swift
+++ 2018/Sources/2018/day05.swift
@@ -7,6 +7,16 @@ func reacts(a: Character, b: Character) -> Bool {
 func 🗓0️⃣5️⃣(input: String, part2: Bool) -> Int {
     var characterArray = [Character]()
 
+    if part2 {
+        var lowest = Int.max
+
+        for character in Set(input.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()) {
+            let result = 🗓0️⃣5️⃣(input: input.filter{String($0) != String(character).lowercased()}.filter{String($0) != String(character).uppercased()}, part2: false)
+            lowest = min(result, lowest)
+        }
+        return lowest
+    }
+
     for character in input.trimmingCharacters(in: .whitespacesAndNewlines) {
         if characterArray.last != nil && reacts(a: character, b: characterArray.last!) {
             characterArray.removeLast()
@@ -22,7 +32,8 @@ assert(🗓0️⃣5️⃣(input: "abBA", part2: false) == 0)
 assert(🗓0️⃣5️⃣(input: "abAB", part2: false) == 4)
 assert(🗓0️⃣5️⃣(input: "aabAAB", part2: false) == 6)
 assert(🗓0️⃣5️⃣(input: "dabAcCaCBAcCcaDA", part2: false) == 10)
+assert(🗓0️⃣5️⃣(input: "dabAcCaCBAcCcaDA", part2: true) == 4)
 
 let input = try String(contentsOfFile: "input05.txt")
 print("🌟 :", 🗓0️⃣5️⃣(input: input, part2: false))
-// print("🌟 :", 🗓0️⃣5️⃣(input: input, part2: true))
+print("🌟 :", 🗓0️⃣5️⃣(input: input, part2: true))
