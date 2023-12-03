23f8cb5717b226b349d73cb18c344c5079914b0d
Adam Barker <mail@steg.pro>
2023-12-03 03:58:39 -0800

    Cleanup

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -3,23 +3,23 @@ use std::collections::HashMap;
 
 
 fn parse(input: &str) -> Vec<Vec<char>> {
-    let map = input.lines().filter(|line: &&str| !line.is_empty())
-                           .map(|line: &str| line.trim())
-                           .collect::<Vec<&str>>()
-                           .into_iter().map(|string: &str| string.chars().collect())
-                           .collect::<Vec<Vec<char>>>();
+    let map: Vec<Vec<char>> = input.lines().filter(|line: &&str| !line.is_empty())
+                                   .map(|line: &str| line.trim())
+                                   .collect::<Vec<&str>>()
+                                   .into_iter().map(|string: &str| string.chars().collect())
+                                   .collect::<Vec<Vec<char>>>();
 
     return map;
 }
 
 fn part1(input: &str) -> u32 {
-    let map = parse(input);
+    let map: Vec<Vec<char>> = parse(input);
 
     let mut digits_stack: String = String::new();
     let mut part_numbers: Vec<u32> = Vec::new();
-    let mut is_part_number = false;
 
     for x_coord in 0 .. map.len() {
+        let mut is_part_number: bool = false;
         for y_coord in 0 .. map[x_coord].len() {
             if !map[x_coord][y_coord].is_ascii_digit() {
                 if is_part_number {
@@ -30,18 +30,8 @@ fn part1(input: &str) -> u32 {
                 continue;
             }
 
-            let mut x_min = 0;
-            if x_coord >= 1 {
-                x_min = x_coord - 1;
-            }
-
-            let mut y_min = 0;
-            if y_coord >= 1 {
-                y_min = y_coord - 1;
-            }
-
-            'outer: for x_jitter in x_min..cmp::min(map.len(), x_coord + 2) {
-                'inter: for y_jitter in y_min..cmp::min(map[x_coord].len(), y_coord + 2) {
+            'outer: for x_jitter in cmp::max(1, x_coord) - 1 .. cmp::min(map.len(), x_coord + 2) {
+                'inter: for y_jitter in cmp::max(1, y_coord) - 1 .. cmp::min(map[x_coord].len(), y_coord + 2) {
                     if map[x_jitter][y_jitter] != '.' && map[x_jitter][y_jitter].is_ascii_punctuation() {
                         is_part_number = true;
                         break 'outer;
@@ -62,13 +52,13 @@ fn part1(input: &str) -> u32 {
 }
 
 fn part2(input: &str) -> u32 {
-    let map = parse(input);
+    let map: Vec<Vec<char>> = parse(input);
 
-    let mut digits_stack = String::new();
+    let mut digits_stack: String = String::new();
     let mut gear_map: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
 
     for x_coord in 0 .. map.len() {
-        let mut is_part_number = false;
+        let mut is_part_number: bool = false;
         let mut gear_coords: (usize, usize) = (0, 0);
         for y_coord in 0 .. map[x_coord].len() {
             if !map[x_coord][y_coord].is_ascii_digit() {
@@ -84,18 +74,8 @@ fn part2(input: &str) -> u32 {
                 continue;
             }
 
-            let mut x_min = 0;
-            if x_coord >= 1 {
-                x_min = x_coord - 1;
-            }
-
-            let mut y_min = 0;
-            if y_coord >= 1 {
-                y_min = y_coord - 1;
-            }
-
-            'outer: for x_jitter in x_min..cmp::min(map.len(), x_coord + 2) {
-                'inter: for y_jitter in y_min..cmp::min(map[x_coord].len(), y_coord + 2) {
+            'outer: for x_jitter in cmp::max(1, x_coord) - 1 .. cmp::min(map.len(), x_coord + 2) {
+                'inter: for y_jitter in cmp::max(1, y_coord) - 1 .. cmp::min(map[x_coord].len(), y_coord + 2) {
                     if map[x_jitter][y_jitter] == '*' {
                         is_part_number = true;
                         gear_coords = (x_jitter, y_jitter);
@@ -117,7 +97,7 @@ fn part2(input: &str) -> u32 {
         digits_stack.clear();
     }
 
-    return gear_map.into_values().filter(|vec| vec.len() == 2).map(|vec| vec.iter().product::<u32>()).sum::<u32>();
+    return gear_map.into_values().filter(|vec: &Vec<u32>| vec.len() == 2).map(|vec: Vec<u32>| vec.iter().product::<u32>()).sum::<u32>();
 }
 
 fn main() {
